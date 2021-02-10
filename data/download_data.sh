537ec699de3c5b6d0258ec25190ffe2570cc1f22
Ryder Bergerud <ryder.bergerud@gmail.com>
2021-02-09 16:35:33 -0800

    Directory structure for data

---
+++ data/download_data.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Need to update requests to include cookie
+# wget -nc https://www.westernpower.co.uk/innovation/pod/dataset/839663bc-5de8-47b3-843c-8a402c47e5a2/resource/ac9384c2-b8c4-4285-8199-801a221926eb/download/pv_train_set0.csv
+# wget -nc https://www.westernpower.co.uk/innovation/pod/dataset/839663bc-5de8-47b3-843c-8a402c47e5a2/resource/dea2f5a6-e1c4-4650-afee-bb9a81a3198b/download/demand_train_set0.csv
